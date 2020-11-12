ae951883bf1418f50b14de0c111d3cd7d53e9c6b
Moses Kamara <kamara.moses@yahoo.com>
2020-11-11 17:33:26 -0800

    update to the index.html file

--- website/app.js
+++ website/app.js
@@ -1,63 +1,122 @@
 /* Global Variables */
-let baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=" ;
-let apikey = "&appid=7439220f89767ecc92468da6aaab2380";
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+// The URL root if user searches by zip code
+const API_ROOT_ZIP = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
-document.getElementById('generate').addEventListener('click', performAction );
-function performAction(e) {
-    const newZipcode = document.getElementById('zip').value;
-    const feel= document.getElementById('feelings').value;
-    getWeatherData(baseURL, newZipcode, apikey)
-    .then(function(data) {
-        postData('http://localhost:8000/projectData' , {
-            date : newDate,
-            temp : data.main.temp,
-            userResponse : feel
+// The URL root if user searches by city
+const API_ROOT_CITY = 'http://api.openweathermap.org/data/2.5/weather?q=';
+
+// The URL for units parameter
+const API_UNITS = '&units=';
+
+// The URL for api key parameter
+const API_KEY = `&appid=${config.API_KEY}`;
+
+// Find the Generate button and add the listener
+const goButton = document.getElementById('generate');
+goButton.addEventListener('click', clickRespond);
+
+// Main function of the program
+// Grabs the user's input, then forms URL, calls API, POSTS and updates UI
+function clickRespond() {
+
+    // Grab user's input
+    const zipInput = document.getElementById('zip');
+    const cityInput = document.getElementById('city');
+    const unitsInput = document.querySelector('input[name="units"]:checked')
+    const feelingsInput = document.getElementById('feelings');
+    let units;
+    let degreeSystem;
+    if (unitsInput) {
+        units = unitsInput.value;
+    } else {
+        units = "metric";
+    }
+    if (units == "metric") {
+        degreeSystem = "C";
+    } else {
+        degreeSystem = "F";
+    }
+
+    // Read values of zip and city
+    const zip = zipInput.value;
+    const city = cityInput.value;
+
+    // Form URL based on zip or city search
+    // (zip takes precendence if both were entered)
+    let url;
+    if (zip) {
+        url = API_ROOT_ZIP + zip + API_UNITS + units + API_KEY;
+    } else if (city) {
+        url = API_ROOT_CITY + city + API_UNITS + units + API_KEY;
+    }
+
+    // Call the API
+    getWeather(url)
+
+        // Prepares data for POST, calls the POST
+        .then(function (weatherData) {
+            const errorMessage = document.getElementById('error');
+            if (weatherData.cod == "200") {
+                errorMessage.classList.add('hide');
+                const icon = weatherData.weather[0].icon;
+                const date = dateTime();
+                const temperature = weatherData.main.temp.toFixed(0);
+                const feelings = feelingsInput.value;
+                postJournal('/add', { icon, date, temperature, feelings });
+
+                // Calls to update the site with latest entry
+                updateUI(degreeSystem);
+
+            } else {
+                console.log('Bad data entered');
+                errorMessage.classList.remove('hide');
+                return;
+            }
         })
-        updateUI();
-    })
 }
 
-const getWeatherData = async (baseURL, newZipcode, apikey) => {
-    const response = await fetch(baseURL+newZipcode+apikey)
-    try{
-        const data = await response.json();
-        return data;
-    }
-    catch(error) {
-        console.log("error", error);
-    };
-};
-const updateUI = async ( url = '') => {
-    // Awaits to retrieve data from app
-    const request = await fetch('/all');
-  
-    try{
-      // Convert to JSON
-      const allData = await request.json();
-      // Select elements and update the divs in HTML
-      document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
-      document.getElementById('content').innerHTML = allData.userResponse;
-      document.getElementById("date").innerHTML =allData.date;
-      // log all new data in the console
-      console.log(allData)
-    // catches the error
-    } catch(error){
-        console.log("error", error);
-    }
-  }
+// Calls the API, converts response to JSON
+// returns weatherData JSON object
+async function getWeather(url) {
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    return weatherData;
+}
 
-async function postData(url, data) {
-    const reponse = await fetch (url, {
-        method : 'postData',
-        credentials :'same-origin',
-        headers : {
-            'Content-Type' : 'application/json'
-        },
-        body : JSON.stringify(data), 
+// POSTs the journal data (icon, date/time, temperature, feelings)
+async function postJournal(url, data) {
+    await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(data)
     });
-    return await response.json();
+}
+
+
+// Updates the website's latest entry card
+// Includes weather icon, date, temperature, feelings
+// Shows the card if it's hidden
+async function updateUI(degreeSystem) {
+    const response = await fetch('/retrieve');
+    const latestEntry = await response.json();
+    document.getElementById('icon').innerHTML = `<img class="icon" src="http://openweathermap.org/img/wn/${latestEntry.icon}@2x.png" alt="Weather icon">`
+    document.getElementById('date').innerHTML = `Date: ${latestEntry.date}`;
+    document.getElementById('temp').innerHTML = `Temperature: ${latestEntry.temperature}\xB0${degreeSystem}`;
+    document.getElementById('content').innerHTML = `Feelings: ${latestEntry.feelings}`;
+    document.getElementById('journal').classList.remove('hide');
+}
+
+// Calculate the user's date and time
+// returns date and time in string
+function dateTime() {
+    const d = new Date();
+    let minutes = d.getMinutes();
+    if (d.getMinutes() <= 9) {
+        minutes = `0${minutes}`;
+    }
+    const date = `${d.getMonth() + 1}.${d.getDate()}.${d.getFullYear()} at time ${d.getHours()}:${minutes}`;
+    return date;
 }
---
+++ website/config-sample.js
@@ -0,0 +1,2 @@
+// Replace value with your own API key
+const config = { API_KEY: '7439220f89767ecc92468da6aaab2380' };
